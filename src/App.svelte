9b62e0a353e96d4e62c9c38eae5d97b39d432d57
Patrick Joyce <pjaudiomv@gmail.com>
2024-08-20 19:43:07 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { Button } from 'flowbite-svelte';
+</script>
+
+<form on:submit={() => console.log('submitted')}>
+    <Button type="submit" disabled={true}>Submit</Button>
+</form>
